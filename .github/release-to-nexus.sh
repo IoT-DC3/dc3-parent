1df5b0224d15775f70e5ee8c840834b91e82e8d1
pnoker <pnokers@gmail.com>
2024-12-05 17:42:53 +0800

    feat: 2024.3.7

--- .github/release-to-nexus.sh
+++ .github/release-to-nexus.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
 WAIT_TIME=10
-sleep "${WAIT_TIME}"
-STAGING_REPO_ID=$(curl -u "${SERVER_USERNAME}:${SERVER_PASSWORD}" \
+
+echo "wait 10s"
+sleep "$WAIT_TIME"
+
+STAGING_REPO_ID=$(curl -u $SERVER_USERNAME:$SERVER_PASSWORD \
                 -X GET \
-                "${SERVER_URL}/service/local/staging/profile_repositories" \
+                "$SERVER_URL/service/local/staging/profile_repositories" \
                 -H "Accept:application/json" \
-                | jq -r '.data | map(select(.type == "open")) | sort_by(.updatedTimestamp) | .[-1].repositoryId')
-if [ -n "${STAGING_REPO_ID}" ]; then
-    curl -v -u ${SERVER_USERNAME}:${SERVER_PASSWORD} \
+                | jq -r ".data | map(select(.type == \"open\")) | sort_by(.updatedTimestamp) | .[-1].repositoryId")
+echo "last staging repository: $STAGING_REPO_ID"
+
+if [ -n "$STAGING_REPO_ID" ]; then
+    echo "close staging repository: $STAGING_REPO_ID"
+    curl -u $SERVER_USERNAME:$SERVER_PASSWORD \
                 -X POST \
-                "${SERVER_URL}/service/local/staging/bulk/close" \
+                "$SERVER_URL/service/local/staging/bulk/close" \
                 -H "Content-Type:application/json" \
-                -d '{"data":{"description":"github action close","stagedRepositoryIds":["${STAGING_REPO_ID}"]}}'
+                -H "Accept:application/json,application/vnd.siesta-error-v1+json,application/vnd.siesta-validation-errors-v1+json" \
+                -d "{\"data\":{\"description\":\"github action close\",\"stagedRepositoryIds\":[\"$STAGING_REPO_ID\"]}}"
+
     while true; do
-        STAGING_REPO_TYPE=$(curl -u "${SERVER_USERNAME}:${SERVER_PASSWORD}" \
+        STAGING_REPO_TYPE=$(curl -u $SERVER_USERNAME:$SERVER_PASSWORD \
                           -X GET \
-                          "${SERVER_URL}/service/local/staging/profile_repositories" \
+                          "$SERVER_URL/service/local/staging/profile_repositories" \
                           -H "Accept:application/json" \
-                          | jq -r '.data | map(select(.repositoryId == "${STAGING_REPO_ID}")) | .[0].type')
-        if [ "${STAGING_REPO_TYPE}" == "closed" ]; then
+                          | jq -r ".data | map(select(.repositoryId == \"$STAGING_REPO_ID\")) | .[0].type")
+        echo "staging repository: $STAGING_REPO_ID, status: $STAGING_REPO_TYPE"
+        if [ "$STAGING_REPO_TYPE" == "closed" ]; then
             break
         fi
-    sleep "${WAIT_TIME}"
+
+    echo "wait 10s"
+    sleep "$WAIT_TIME"
     done
-    curl -v -u ${SERVER_USERNAME}:${SERVER_PASSWORD} \
+
+    echo "release staging repository: $STAGING_REPO_ID"
+    curl -u $SERVER_USERNAME:$SERVER_PASSWORD \
                 -X POST \
-                "https://s01.oss.sonatype.org/service/local/staging/bulk/promote" \
+                "$SERVER_URL/service/local/staging/bulk/promote" \
                 -H "Content-Type:application/json" \
-                -d '{"data":{"description":"github action release","autoDropAfterRelease":true,"stagedRepositoryIds":["${STAGING_REPO_ID}"]}}'
+                -d "{\"data\":{\"description\":\"github action release\",\"autoDropAfterRelease\":true,\"stagedRepositoryIds\":[\"$STAGING_REPO_ID\"]}}"
 fi
