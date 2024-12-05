50d85064c0d1a97c1d89135eb53f771ae61cc566
Hongyuan Zhang <pnokers@gmail.com>
2024-12-05 16:16:22 +0800

    Create release-to-nexus.sh

---
+++ .github/release-to-nexus.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+WAIT_TIME=10
+sleep "${WAIT_TIME}"
+STAGING_REPO_ID=$(curl -u "${env.SERVER_USERNAME}:${env.SERVER_PASSWORD}" \
+                -X GET \
+                "${env.SERVER_URL}/service/local/staging/profile_repositories" \
+                -H "Accept:application/json" \
+                | jq -r '.data | map(select(.type == "open")) | sort_by(.updatedTimestamp) | .[-1].repositoryId')
+if [ -n "${STAGING_REPO_ID}" ]; then
+    curl -v -u ${env.SERVER_USERNAME}:${env.SERVER_PASSWORD} \
+                -X POST \
+                "${env.SERVER_URL}/service/local/staging/bulk/close" \
+                -H "Content-Type:application/json" \
+                -d '{"data":{"description":"github action close","stagedRepositoryIds":["${STAGING_REPO_ID}"]}}'
+    while true; do
+        STAGING_REPO_TYPE=$(curl -u "${env.SERVER_USERNAME}:${env.SERVER_PASSWORD}" \
+                          -X GET \
+                          "${env.SERVER_URL}/service/local/staging/profile_repositories" \
+                          -H "Accept:application/json" \
+                          | jq -r '.data | map(select(.repositoryId == "${STAGING_REPO_ID}")) | .[0].type')
+        if [ "${STAGING_REPO_TYPE}" == "closed" ]; then
+            break
+        fi
+    sleep "${WAIT_TIME}"
+    done
+    curl -v -u ${env.SERVER_USERNAME}:${env.SERVER_PASSWORD} \
+                -X POST \
+                "https://s01.oss.sonatype.org/service/local/staging/bulk/promote" \
+                -H "Content-Type:application/json" \
+                -d '{"data":{"description":"github action release","autoDropAfterRelease":true,"stagedRepositoryIds":["${STAGING_REPO_ID}"]}}'
+fi
